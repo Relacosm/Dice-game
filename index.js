1d4909e8eef41cc04cdf5128d1f2f19c76a4592a
Relacosm <hraj51399@gmail.com>
2024-09-17 21:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 randomnumber1=Math.floor(Math.random()*6)+1;
-var dm1="/images/dice"+randomnumber1+".png";
+var dm1="./images/dice"+randomnumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",dm1);
 
 randomnumber2=Math.floor(Math.random()*6)+1;
-var dm2="/images/dice"+randomnumber2+".png";
+var dm2="./images/dice"+randomnumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",dm2);
 
 if(randomnumber1>randomnumber2){
